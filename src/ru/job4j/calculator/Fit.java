b8b75aa00edf1bd880af48fac2064d5c48863b0f
Prometheus <pointofview88@gmail.com>
2021-06-01 14:24:38 +0300

    6. Идеальный вес [#41584]

---
+++ src/ru/job4j/calculator/Fit.java
@@ -0,0 +1,22 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+    public static double manWeight(short height) {
+        return (height - 100) * 1.15;
+    }
+
+    public static double womanWeight(short height) {
+        return (height - 110) * 1.15;
+    }
+
+    public static void main(String[] args) {
+        short manHeight = 187;
+        double man = Fit.manWeight(manHeight);
+        System.out.println("Man 187 is " + man);
+
+        short womanHeight = 170;
+        double woman = Fit.womanWeight(womanHeight);
+        System.out.println("Woman 170 is " + woman);
+    }
+}
