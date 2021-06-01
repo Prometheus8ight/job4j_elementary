08991172ee4a18f9b6aa512dee68e9280117977d
Prometheus <pointofview88@gmail.com>
2021-06-01 11:29:21 +0300

    3.3. Разбор первой программы. [#159033]

---
+++ src/ru/job4j/Multiply.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class Multiply {
+    public static void main(String[] args) {
+        int num = 1;
+        for(int i = 1; i <= 10; i++){
+            System.out.println(num + " * " + i + " = " + num * i);
+        }
+    }
+}
