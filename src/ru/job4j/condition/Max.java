8dbaf9b662807eb0e43f669a2bc1b5c558c56c8a
Prometheus8ight <pointofview8*@gmai.com>
2021-07-11 12:19:24 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,8 +1,17 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        int result = left > right ? left : right;
-        return result;
+    public static int max(int a, int b) {
+        return a > b ? a : b;
+    }
+
+    public static int max(int a, int b, int c) {
+        int max = max(a, b);
+        return max > c ? max : c;
+    }
+
+    public static int max(int a, int b, int c, int d) {
+        int max = max(a, b, c);
+        return max > d ? max : d;
     }
 }
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -40,4 +40,46 @@ public class MaxTest {
         int expected = 50;
         Assert.assertEquals(result, expected);
     }
+
+    @Test
+    public void whenMax1To2To3Then3() {
+        int a = 1;
+        int b = 3;
+        int c = 2;
+        int result = Max.max(a, b, c);
+        int expected = 3;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax20To7To2Then20() {
+        int a = 20;
+        int b = 7;
+        int c = 2;
+        int result = Max.max(a, b, c);
+        int expected = 20;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax10To20To3To100Then100() {
+        int a = 10;
+        int b = 20;
+        int c = 3;
+        int d = 100;
+        int result = Max.max(a, b, c, d);
+        int expected = 100;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax10To200To3To111Then200() {
+        int a = 10;
+        int b = 200;
+        int c = 3;
+        int d = 111;
+        int result = Max.max(a, b, c, d);
+        int expected = 200;
+        Assert.assertEquals(result, expected);
+    }
 }
